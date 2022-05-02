5f404ac0317ddef4fdc5d2c9ab5359b0ec9dbad6
airgedon <ssmit7241@gmail.com>
2022-05-02 13:06:20 +0600

    modified urls.py

--- account_security/urls.py
+++ account_security/urls.py
@@ -7,20 +7,16 @@ from twofa import views as twofa_views
 from phone_verification import views as verify_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+                  path('login/', auth_views.LoginView.as_view(), name='login'),
+                  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('register/', twofa_views.register, name='register'),
-    path('2fa/', twofa_views.twofa, name='2fa'),
-    path('token/sms', twofa_views.token_sms, name='token-sms'),
-    path('token/voice', twofa_views.token_voice, name='token-voice'),
-    path('token/onetouch', twofa_views.token_onetouch, name='token-onetouch'),  # noqa: E501
-    path('protected/', twofa_views.protected, name='protected'),
-    path('onetouch-status', twofa_views.onetouch_status, name='onetouch-status'),  # noqa: E501
+                  path('register/', twofa_views.register, name='register'),
+                  path('2fa/', twofa_views.twofa, name='2fa'),  # green
+                  path('token/sms', twofa_views.token_sms, name='token-sms'),
+                  path('token/voice', twofa_views.token_voice, name='token-voice'),
+                  path('token/onetouch', twofa_views.token_onetouch, name='token-onetouch'),  # noqa: E501
+                  path('protected/', twofa_views.protected, name='protected'),
+                  path('onetouch-status', twofa_views.onetouch_status, name='onetouch-status'),  # noqa: E501
 
-    path('verification/', verify_views.phone_verification, name='phone_verification'),  # noqa: E501
-    path('verification/token/', verify_views.token_validation, name='token_validation'),  # noqa: E501
-    path('verified/', verify_views.verified, name='verified'),
-
-    path('', twofa_views.home, name='home'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('', twofa_views.home, name='home'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
