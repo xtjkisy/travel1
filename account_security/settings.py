cd6ddd7881079df8bc6a716324021a905c48d3c3
xtjkisy <syrga.usupova@alatoo.edu.kg>
2022-05-24 13:05:39 +0600

    gggggggg

--- account_security/settings.py
+++ account_security/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'widget_tweaks',
     'twofa',
     'phone_verification',
     'clients'
